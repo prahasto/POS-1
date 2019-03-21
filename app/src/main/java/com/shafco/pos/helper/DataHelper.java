55e3d75fa714154659fddcfdd941e5ad9a0cdb88
prahastoindraaji@gmail.com <naresh060408>
2019-03-21 16:48:16 +0700

    public void onCreate(SQLiteDatabase db) {         db.execSQL(db_create_tbl_r_produk);         db.execSQL(db_create_tbl_t_transpos);         db.execSQL(db_create_tbl_t_transpos_detail);         db.execSQL(db_create_tbl_t_transpos_bayar);         db.execSQL(db_create_tbl_m_carabyar);         db.execSQL(db_create_tbl_m_edc);
        }

--- app/src/main/java/com/shafco/pos/helper/DataHelper.java
+++ app/src/main/java/com/shafco/pos/helper/DataHelper.java
@@ -70,75 +70,19 @@ public class DataHelper extends SQLiteOpenHelper {
     public static final String COLUMN_TIPEBANK = "tipe_bank";
     public static final String COLUMN_R_ORGANISASI_ID_EDC = "r_organisasi_id";
 
-    private static final String db_create_tbl_r_produk = "create table "
-            + TABLE_R_PRODUK + "("
-            + COLUMN_R_PRODUK_ID + " integer primary key autoincrement, "
-            + COLUMN_KODE_PRODUK + " varchar(20) , "
-            + COLUMN_HPJ + " integer,"
-            + COLUMN_NAMA_PRODUK + " varchar(100))";
 
-    private static final String db_create_tbl_t_transpos = "create table "
-            + TABLE_T_TRANSPOS + "("
-            + COLUMN_T_TRANSPOS_ID + " integer primary key autoincrement, "
-            + COLUMN_NOTRANS + " varchar(30) , "
-            + COLUMN_TANGGAL + " date,"
-            + COLUMN_TOTAL_TRANS + " integer,"
-            + COLUMN_NO_FAKTUR_PAJAK + " varchar(30) , "
-            + COLUMN_R_TIPEDOKUMEN_ID + " integer , "
-            + COLUMN_KDSTASTUSDOKUMEN + " varchar(5) , "
-            + COLUMN_R_ORGANISASI_ID + " integer,"
-            + COLUMN_IS_UPLOAD + " integer,"
-            + COLUMN_JAM + " time)";
-
-    private static final String db_create_tbl_t_transpos_detail = "create table "
-            + TABLE_T_TRANSPOS_DETAIL + "("
-            + COLUMN_T_TRANSPOS_DETAIL_ID + " integer primary key autoincrement, "
-            + COLUMN_NOTRANS_DETAIL + " varchar(30) , "
-            + COLUMN_KDPRODUK + " varchar(30) , "
-            + COLUMN_QTY + " integer,"
-            + COLUMN_HARGA + " integer,"
-            + COLUMN_PRSN_DISC + " double , "
-            + COLUMN_NOMINAL_DISC + " integer , "
-            + COLUMN_NETTO + " integer , "
-            + COLUMN_PPN + " double , "
-            + COLUMN_R_PRODUK_ID_TRANPOS_DETAIL + " integer,"
-            + COLUMN_M_KATEGORI_DISC_ID + " integer)";
-
-    private static final String db_create_tbl_t_transpos_bayar = "create table "
-            + TABLE_T_BAYAR_POS + "("
-            + COLUMN_NOTRANS_BAYAR_POS_id + " integer primary key autoincrement, "
-            + COLUMN_NOTRANS_BAYAR_POS + " varchar(30) , "
-            + COLUMN_KODE_BAYAR_POS + " varchar(30) , "
-            + COLUMN_M_EDC_ID + " integer,"
-            + COLUMN_NOMINAL_BAYAR + " integer,"
-            + COLUMN_BAYAR + " integer)";
-
-
-    private static final String db_create_tbl_m_carabyar = "create table "
-            + TABLE_M_CARABAYAR + "("
-            + COLUMN_CARABAYAR_ID + "integer primary key autoincrement, "
-            + COLUMN_CARABAYAR + " varchar(20) , "
-            + COLUMN_NAMA_CARABAYAR + "  varchar(20),"
-            + COLUMN_ISEDC + " varchar(100))";
-
-    private static final String db_create_tbl_m_edc = "create table "
-            + TABLE_M_EDC + "("
-            + COLUMN_NAMAMERCHAND_id + "integer primary key autoincrement, "
-            + COLUMN_NAMAMERCHAND + " varchar(20) , "
-            + COLUMN_TIPEBANK + "  varchar(50),"
-            + COLUMN_R_ORGANISASI_ID_EDC + " integer)";
     public DataHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         // TODO Auto-generated constructor stub s
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(db_create_tbl_r_produk);
-        db.execSQL(db_create_tbl_t_transpos);
-        db.execSQL(db_create_tbl_t_transpos_detail);
-        db.execSQL(db_create_tbl_t_transpos_bayar);
-        db.execSQL(db_create_tbl_m_carabyar);
-        db.execSQL(db_create_tbl_m_edc);
+      //  db.execSQL(db_create_tbl_r_produk);
+       // db.execSQL(db_create_tbl_t_transpos);
+       // db.execSQL(db_create_tbl_t_transpos_detail);
+       // db.execSQL(db_create_tbl_t_transpos_bayar);
+        //db.execSQL(db_create_tbl_m_carabyar);
+        //db.execSQL(db_create_tbl_m_edc);
 
     }
 
