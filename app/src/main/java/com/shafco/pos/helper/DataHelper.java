2cc6f9651e4d44247b34313b5810ae7942666599
prahastoindraaji@gmail.com <naresh060408>
2019-03-21 13:40:29 +0700

    add table produl tranpos tranpos detail

--- app/src/main/java/com/shafco/pos/helper/DataHelper.java
+++ app/src/main/java/com/shafco/pos/helper/DataHelper.java
@@ -10,9 +10,40 @@ public class DataHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "pos.db";
     private static final int DATABASE_VERSION = 1;
 
+    public static final String TABLE_R_PRODUK = "r_produk";
+    public static final String COLUMN_KODE_PRODUK = "kdproduk";
+    public static final String COLUMN_NAMA_PRODUK = "nmproduk";
+    public static final String COLUMN_HPJ = "hpj";
+    public static final String COLUMN_R_PRODUK_ID = "r_produk_id";
+
+    public static final String TABLE_T_TRANSPOS = "t_transpos";
+    public static final String COLUMN_NOTRANS = "notrans";
+    public static final String COLUMN_TANGGAL = "tanggal";
+    public static final String COLUMN_TOTAL_TRANS = "total_trans";
+    public static final String COLUMN_NO_FAKTUR_PAJAK = "nofaktur_pajak";
+    public static final String COLUMN_R_TIPEDOKUMEN_ID= "r_tipedokumen_id";
+    public static final String COLUMN_KDSTASTUSDOKUMEN= "kdstatusdokumen";
+    public static final String COLUMN_R_ORGANISASI_ID= "r_organisasi_id";
+    public static final String COLUMN_JAM= "jam";
+
+    public static final String TABLE_T_TRANSPOS_DETAIL = "t_transpos_detail";
+    public static final String COLUMN_NOTRANS_DETAIL = "notrans";
+    public static final String COLUMN_KDPRODUK = "kdproduk";
+    public static final String COLUMN_QTY = "qty";
+    public static final String COLUMN_HARGA = "harga";
+    public static final String COLUMN_PRSN_DISC = "prsn_disc";
+    public static final String COLUMN_NOMINAL_DISC = "nominal_disc";
+    public static final String COLUMN_NETTO = "netto";
+    public static final String COLUMN_PPN = "ppn";
+    public static final String COLUMN_R_PRODUK_ID_TRANPOS_DETAIL = "r_produk_id";
+    public static final String COLUMN_M_KATEGORI_DISC_ID = "m_kategori_disc_id";
+
+
+
+
     public DataHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        // TODO Auto-generated constructor stub
+        // TODO Auto-generated constructor stub s
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
